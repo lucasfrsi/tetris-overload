2bea8b9b88643693c06baba4e548a77d626639da
lucasfrsi <lucasfrsi@gmail.com>
2021-06-12 16:58:04 -0300

    Add style to match main stage

--- src/components/PieceHolder/style.js
+++ src/components/PieceHolder/style.js
@@ -2,16 +2,10 @@ import styled from 'styled-components';
 
 export const StyledPieceHolder = styled.div`
   display: grid;
-  grid-template-rows: repeat(
-    ${(props) => props.height},
-    calc(5vw / ${(props) => props.width})
-  );
-  grid-template-columns: repeat(${(props) => props.width}, 1fr);
-  /* grid-gap: 1px; */
+  grid-template-rows: repeat(${(props) => props.height}, calc(100vh / 35));
+  grid-template-columns: repeat(${(props) => props.width}, calc(100vh / 35));
   border: 2px solid #333;
-  width: 100%;
-  max-width: 5vw;
   background: #111;
 
-  margin-left: auto;
+  margin: 0 2rem 0;
 `;
