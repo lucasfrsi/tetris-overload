7cc5d021e38e85583a8cfd67fa7b99ca960b7ce1
lucasfrsi <lucasfrsi@gmail.com>
2021-06-14 16:53:17 -0300

    Add Press Start 2P font

--- src/globalHelmet.jsx
+++ src/globalHelmet.jsx
@@ -10,6 +10,7 @@ const GlobalHelmet = () => (
     {/* Fonts */}
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" />
   </Helmet>
 );
 
