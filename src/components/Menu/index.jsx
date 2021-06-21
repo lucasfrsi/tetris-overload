255dee781ca9f1c3820b87f4591636b58ad9fa3f
lucasfrsi <lucasfrsi@gmail.com>
2021-06-21 19:02:16 -0300

    Remove redux related code

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { setInGameAction } from 'store/actions/tetris';
+import PropTypes from 'prop-types';
 import sfxOn from 'assets/sfx_on.svg';
 import sfxOff from 'assets/sfx_off.svg';
 import musicOn from 'assets/music_on.svg';
@@ -8,43 +7,42 @@ import musicOff from 'assets/music_off.svg';
 
 import * as styles from './style';
 
-const Menu = () => {
-  const [sfx, setSfx] = useState(false);
-  const [music, setMusic] = useState(false);
-
-  const dispatch = useDispatch();
-
-  const startGame = () => {
-    dispatch(setInGameAction(true));
-  };
-
-  return (
-    <div css={styles.menu}>
-      <span css={styles.title}>Tetris</span>
-      <span css={styles.title}>Overload</span>
-      <button type="button" onClick={startGame}>Play</button>
-      <button type="button">Options</button>
-      <button type="button">Credits</button>
-      <div>
-        <img
-          role="presentation"
-          src={sfx ? sfxOff : sfxOn}
-          alt={sfx ? 'sfx off' : 'sfx on'}
-          draggable={false}
-          onClick={() => setSfx((prev) => !prev)}
-          onKeyDown={() => {}}
-        />
-        <img
-          role="presentation"
-          src={music ? musicOff : musicOn}
-          alt={music ? 'music off' : 'music on'}
-          draggable={false}
-          onClick={() => setMusic((prev) => !prev)}
-          onKeyDown={() => {}}
-        />
-      </div>
+const Menu = ({ play, SFX, BGM, toggleSFX, toggleBGM }) => (
+  <div css={styles.menu}>
+    <span css={styles.title}>Tetris</span>
+    <span css={styles.title}>Overload</span>
+    <button type="button" onClick={play}>Play</button>
+    <button type="button">Options</button>
+    <button type="button">Credits</button>
+    <div>
+      <h3>BGM: {BGM ? 'muted' : 'unmuted'}</h3>
+      <h3>SFX: {SFX ? 'muted' : 'unmuted'}</h3>
+      <img
+        role="presentation"
+        src={SFX ? sfxOff : sfxOn}
+        alt={SFX ? 'sfx off' : 'sfx on'}
+        draggable={false}
+        onClick={toggleSFX}
+        onKeyDown={() => {}}
+      />
+      <img
+        role="presentation"
+        src={BGM ? musicOff : musicOn}
+        alt={BGM ? 'music off' : 'music on'}
+        draggable={false}
+        onClick={toggleBGM}
+        onKeyDown={() => {}}
+      />
     </div>
-  );
+  </div>
+);
+
+Menu.propTypes = {
+  play: PropTypes.func.isRequired,
+  SFX: PropTypes.bool.isRequired,
+  BGM: PropTypes.bool.isRequired,
+  toggleSFX: PropTypes.func.isRequired,
+  toggleBGM: PropTypes.func.isRequired,
 };
 
 export default Menu;
