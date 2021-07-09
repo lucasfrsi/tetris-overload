2c8afeece000846652f2e58ef52a26606befd5ab
lucasfrsi <lucasfrsi@gmail.com>
2021-07-09 20:55:02 -0300

    Stop bgm on gameover and play on start

--- src/hooks/useTetris.js
+++ src/hooks/useTetris.js
@@ -1,9 +1,8 @@
 import { useInterval } from 'hooks/useInterval';
-
 import { checkCollision, createMainStage } from 'utils/gameHelpers';
 import { TETROMINO_MERGE, TETROMINO_MOVE } from 'utils/SFXPaths';
 
-export const useTetris = ({ skillsAPI, gameStatusAPI, playerAPI, stageAPI, SFX_API }) => {
+export const useTetris = ({ skillsAPI, gameStatusAPI, playerAPI, stageAPI, SFX_API, BGM_API }) => {
   const {
     state: {
       player,
@@ -43,6 +42,9 @@ export const useTetris = ({ skillsAPI, gameStatusAPI, playerAPI, stageAPI, SFX_A
       paused,
       timeStop,
     },
+    actions: {
+      resetSkills,
+    },
   } = skillsAPI;
 
   const {
@@ -51,6 +53,13 @@ export const useTetris = ({ skillsAPI, gameStatusAPI, playerAPI, stageAPI, SFX_A
     },
   } = SFX_API;
 
+  const {
+    actions: {
+      playBGM,
+      stopBGM,
+    },
+  } = BGM_API;
+
   const movePlayer = (xDir, yDir = 0) => {
     if (!checkCollision(player, stage, { x: xDir, y: yDir })) {
       updatePlayerPos({ x: xDir, y: yDir });
@@ -58,16 +67,28 @@ export const useTetris = ({ skillsAPI, gameStatusAPI, playerAPI, stageAPI, SFX_A
     }
   };
 
-  const startGame = () => {
-    // Reset everything
+  const resetGame = () => {
+    resetSkills();
     setStage(createMainStage());
-    setDropTime(1000);
+    setGameOver(false);
     resetPlayer();
     setScore(0);
     setLevel(0);
     setRows(0);
-    setGameOver(false);
+    setDropTime(null);
+    stopBGM();
   };
+
+  const startGame = () => {
+    resetGame();
+    setDropTime(1000);
+    playBGM();
+  };
+
+  // separate start game from reset game
+  // reset everything when leaving ingame
+  // popup for confirmation when leaving
+  // maybe removing start button once game is running?
 
   const drop = () => {
     // Increase level when player has cleared 10 rows
@@ -79,12 +100,12 @@ export const useTetris = ({ skillsAPI, gameStatusAPI, playerAPI, stageAPI, SFX_A
 
     if (!checkCollision(player, stage, { x: 0, y: 1 })) {
       updatePlayerPos({ x: 0, y: 1, collided: false });
-      // playSFX(TETROMINO_MOVE);
     } else {
       // Game over!
       if (player.pos.y < 1) {
         setGameOver(true);
         setDropTime(null);
+        stopBGM();
       }
       updatePlayerPos({ x: 0, y: 0, collided: !timeStop.active });
       playSFX(TETROMINO_MERGE);
