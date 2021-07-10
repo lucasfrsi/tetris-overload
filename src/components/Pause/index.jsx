9fa99aa4d6eef3d170f187717bfbe15145b038b5
lucasfrsi <lucasfrsi@gmail.com>
2021-07-10 18:15:41 -0300

    Put condition to show sub message

--- src/components/Pause/index.jsx
+++ src/components/Pause/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import * as styles from './style';
 
-const Pause = () => (
+const Pause = ({ dialog }) => (
   <div css={styles.backdrop}>
     <span css={styles.main}>PAUSED</span>
-    <span css={styles.sub}>Press &quot;P&quot; to resume</span>
+    {!dialog && <span css={styles.sub}>Press &quot;P&quot; to resume</span>}
   </div>
 );
+
+Pause.propTypes = {
+  dialog: PropTypes.bool.isRequired,
+};
 
 export default Pause;
