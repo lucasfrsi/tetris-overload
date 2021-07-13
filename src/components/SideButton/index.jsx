5c61bb61f780979ae14f9f16b90827261b07af24
lucasfrsi <lucasfrsi@gmail.com>
2021-07-13 07:21:58 -0300

    Change sfx sound of play button

--- src/components/SideButton/index.jsx
+++ src/components/SideButton/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BUTTON_HOVER, BUTTON_SELECT } from 'utils/SFXPaths';
+import { BUTTON_HOVER, BUTTON_SELECT, BUTTON_START } from 'utils/SFXPaths';
 
 import * as styles from './style';
 
-const SideButton = ({ buttonName, onClick, playSFX, disabled, playSFXOnClick }) => (
+const SideButton = ({ buttonName, onClick, playSFX, disabled, playSFXOnClick, start }) => (
   <button
     type="button"
     tabIndex={-1}
     css={styles.sideButton}
     onClick={() => {
       onClick();
-      if (playSFXOnClick) playSFX(BUTTON_SELECT);
+      if (playSFXOnClick) playSFX(start ? BUTTON_START : BUTTON_SELECT);
     }}
     onMouseEnter={() => playSFX(BUTTON_HOVER)}
     disabled={disabled}
@@ -26,11 +26,13 @@ SideButton.propTypes = {
   playSFX: PropTypes.func.isRequired,
   disabled: PropTypes.bool,
   playSFXOnClick: PropTypes.bool,
+  start: PropTypes.bool,
 };
 
 SideButton.defaultProps = {
   disabled: false,
   playSFXOnClick: true,
+  start: false,
 };
 
 export default SideButton;
