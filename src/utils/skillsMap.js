639c625de8b44ab180dddabb0b79582b57635d2d
lucasfrsi <lucasfrsi@gmail.com>
2021-07-15 21:51:12 -0300

    Create a skill map with names and icon imports

--- src/utils/skillsMap.js
+++ src/utils/skillsMap.js
@@ -1,3 +1,5 @@
+import icon from 'assets/icons/time_stop.svg';
+
 export const CLAIRVOYANCE = 'Clairvoyance';
 export const PIXELPOCKET = 'Pixel Pocket';
 export const INTUITION = 'Intuition';
@@ -5,3 +7,13 @@ export const BLINK = 'Blink';
 export const TIMESTOP = 'Time Stop';
 export const MIMIC = 'Mimic';
 export const PERFECTIONISM = 'Perfectionism';
+
+export default {
+  [CLAIRVOYANCE]: icon,
+  [PIXELPOCKET]: icon,
+  [INTUITION]: icon,
+  [BLINK]: icon,
+  [TIMESTOP]: icon,
+  [MIMIC]: icon,
+  [PERFECTIONISM]: icon,
+};
