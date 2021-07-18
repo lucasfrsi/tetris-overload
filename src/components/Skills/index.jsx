7fd47d38914e1068e5a3f606f55f10ab6524502e
lucasfrsi <lucasfrsi@gmail.com>
2021-07-18 14:51:39 -0300

    Add skill progression

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -5,14 +5,22 @@ import * as styles from './style';
 
 const Skills = ({ skills, canSkillBeLeveled, levelUpSkill }) => (
   <div css={styles.skills}>
-    {skills.map((skill) => (
-      <Skill
-        key={skill.name}
-        skill={skill}
-        canSkillBeLeveled={canSkillBeLeveled}
-        levelUpSkill={levelUpSkill}
-      />
-    ))}
+    {skills.map((skill, index) => {
+      const previousSkill = skills[index - 1];
+      let shouldSkillBeRendered = false;
+      if (previousSkill === undefined || previousSkill.currentLevel >= 1) {
+        shouldSkillBeRendered = true;
+      }
+
+      return shouldSkillBeRendered ? (
+        <Skill
+          key={skill.name}
+          skill={skill}
+          canSkillBeLeveled={canSkillBeLeveled}
+          levelUpSkill={levelUpSkill}
+        />
+      ) : null;
+    })}
   </div>
 );
 
