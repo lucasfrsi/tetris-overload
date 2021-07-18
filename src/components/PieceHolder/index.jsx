e937ca2b436813ff8272c078e71f4c05ee631b83
lucasfrsi <lucasfrsi@gmail.com>
2021-07-18 12:59:25 -0300

    Refactor and style pieceholder component

--- src/components/PieceHolder/index.jsx
+++ src/components/PieceHolder/index.jsx
@@ -4,16 +4,19 @@ import Cell from 'components/Cell';
 import { StyledPieceHolder } from './style';
 
 const PieceHolder = ({ pieceHolderStage }) => (
-  pieceHolderStage.length
-  && (
+  pieceHolderStage ? (
     <StyledPieceHolder width={pieceHolderStage[0].length} height={pieceHolderStage.length}>
       {pieceHolderStage.map((row) => row.map((cell, x) => <Cell key={x} type={cell} />))}
     </StyledPieceHolder>
-  )
+  ) : null
 );
 
 PieceHolder.propTypes = {
-  pieceHolderStage: PropTypes.arrayOf(PropTypes.array).isRequired,
+  pieceHolderStage: PropTypes.arrayOf(PropTypes.array),
+};
+
+PieceHolder.defaultProps = {
+  pieceHolderStage: null,
 };
 
 export default PieceHolder;
--- src/components/PieceHolder/style.js
+++ src/components/PieceHolder/style.js
@@ -4,12 +4,8 @@ export const StyledPieceHolder = styled.div`
   display: grid;
   grid-template-rows: repeat(${(props) => props.height}, calc(100vh / 35));
   grid-template-columns: repeat(${(props) => props.width}, calc(100vh / 35));
-  border: 2px solid #333;
-  background: #111;
-
-  margin: 0 2rem 0;
-  padding: 28%;
-  width: 20vw;
-  height: 20vw;
-  overflow: hidden;
+  
+  width: fit-content;
+  margin: auto;
+  /* background: red; */
 `;
