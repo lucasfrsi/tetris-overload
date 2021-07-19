de0348a50e0b060b7540f0f1e81efa9c765facff
lucasfrsi <lucasfrsi@gmail.com>
2021-07-19 19:58:08 -0300

    Remove slice, in favor of logic check inside map fn

--- src/components/Stage/index.jsx
+++ src/components/Stage/index.jsx
@@ -5,15 +5,16 @@ import Cell from 'components/Cell';
 
 import { StyledStage } from './style';
 
-const Stage = ({ stage }) => {
-  const visibleStage = stage.slice(1);
-
-  return (
-    <StyledStage width={visibleStage[0].length} height={visibleStage.length}>
-      {visibleStage.map((row) => row.map((cell, x) => <Cell key={x} type={cell} />))}
-    </StyledStage>
-  );
-};
+const Stage = ({ stage }) => (
+  // The height does not take into account the sky (index 0)
+  <StyledStage width={stage[0].length} height={stage.length - 1}>
+    {stage.map((row, index) => {
+      // Ignore rendering the sky (index 0)
+      if (index === 0) return null;
+      return row.map((cell, i) => <Cell key={i} type={cell} />);
+    })}
+  </StyledStage>
+);
 
 Stage.propTypes = {
   stage: PropTypes.arrayOf(PropTypes.array).isRequired,
