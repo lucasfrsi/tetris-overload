1bc1cef915f381393735821b100d1fe88b7869b1
lucasfrsi <lucasfrsi@gmail.com>
2021-07-23 09:30:01 -0300

    Style picker component

--- src/components/Picker/index.jsx
+++ src/components/Picker/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BUTTON_TOGGLE } from 'utils/SFXPaths';
+import { StyledPicker } from './style';
 
-const Picker = ({ state, changeState, possibleStates }) => {
+const Picker = ({ state, changeState, possibleStates, playSFX }) => {
   const currentChoice = state;
   const currentChoicePosition = possibleStates.indexOf(currentChoice);
   const choicesLength = possibleStates.length;
@@ -12,26 +14,29 @@ const Picker = ({ state, changeState, possibleStates }) => {
     } else if ((side === 'right') && (currentChoicePosition < choicesLength - 1)) {
       changeState(possibleStates[currentChoicePosition + 1]);
     }
+    playSFX(BUTTON_TOGGLE);
   };
 
   return (
-    <div>
+    <StyledPicker>
       <button
         type="button"
         onClick={() => onClickHandler('left')}
+        tabIndex={-1}
         disabled={currentChoicePosition === 0}
       >
         &lt;
       </button>
-      {currentChoice}
+      <span>{currentChoice}</span>
       <button
         type="button"
         onClick={() => onClickHandler('right')}
+        tabIndex={-1}
         disabled={currentChoicePosition === choicesLength - 1}
       >
         &gt;
       </button>
-    </div>
+    </StyledPicker>
   );
 };
 
@@ -39,6 +44,7 @@ Picker.propTypes = {
   state: PropTypes.string.isRequired,
   changeState: PropTypes.func.isRequired,
   possibleStates: PropTypes.arrayOf(PropTypes.string).isRequired,
+  playSFX: PropTypes.func.isRequired,
 };
 
 export default Picker;
